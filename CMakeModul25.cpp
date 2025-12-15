8ce9d141729cddbf9622c6b4fdd392aab3440060
Linad43 <merzlikindaniil97@gmail.com>
2025-12-15 19:20:29 +0300

    Добавьте файлы проекта.

---
+++ CMakeModul25.cpp
@@ -0,0 +1,12 @@
+﻿// CMakeModul25.cpp: определяет точку входа для приложения.
+//
+
+#include "main_header.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
